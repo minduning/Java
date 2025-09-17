7bd50c4ef7fa18f9ec450c89c9a6f5f1784a3187
minduning <conan20700@gmail.com>
2025-09-17 18:30:58 +0900

    Update boj_2884.java

--- PS/BOJ/조건문/boj_2884.java
+++ PS/BOJ/조건문/boj_2884.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
 
-        if (B >= 45) {
-            System.out.println(A + " " + (B - 45));
-        } else if (A == 0 && B < 45) {
-            System.out.println(23 + " " + (B + 15));
-        } else {
-            System.out.println((A - 1) + " " + (B + 15));
+        if (minute >= 45) {
+            System.out.println(hour + " " + (minute - 45));
+        } else if (hour == 0 && minute < 45) {
+            System.out.println(23 + " " + (minute + 15));
+        } else if (hour != 0 && minute < 45) {
+            System.out.println((hour - 1) + " " + (minute + 15));
         }
 
         sc.close();
