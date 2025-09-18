f71d18b91954d809f37967aa4961399737591c71
minduning <conan20700@gmail.com>
2025-09-18 22:41:34 +0900

    Create boj_25314.java

---
+++ PS/BOJ/반복문/boj_25314.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        for (int i = 0; i < (count / 4); i++) {
+            System.out.print("long ");
+        }
+
+        System.out.println("int");
+
+        sc.close();
+    }
+}
